300106030edde42eaa4112a18a45222474deb269
ydc91 <61168057+ydc91@users.noreply.github.com>
2020-02-21 11:45:10 -0600

    color

--- parent-component.js
+++ parent-component.js
@@ -4,26 +4,35 @@ import './child-component'
 
 class ParentComponent extends LitElement{
     static get properties(){
-        return { childMessage:{type: String}
+        return { childMessage:{type: String},
+        propColor:{type: String}
       }
     }
     render(){
         return html`
           <h1> ${this.childMessage} </h1>
           <h3>Jugando con colores</h3>
-          <child-component childMessage2="CHILD COMPONENT">
+          <child-component childMessage2="CHILD COMPONENT" @someone-push-me="${this.someonePush}">
           </my-second-component>
           `;
+    }
+
+    someonePush(event) {
+      this.propColor = (event.detail.el.propBtnColor);
     }
     static get styles(){
         return css`
         :host{
             display: block;
             padding: 8px;
-            background-color: var(--parent-component-background-color);
+            background-color: var('${this.propColor}');
             text-align: center;
         }
         `
+    }
+    constructor() {
+      super();
+      this.propColor = 'green';
     }
 
 }
